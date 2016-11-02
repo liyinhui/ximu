42c816b8e34192b74199c3f431104a05b603efff
liyinhui <1561164137@qq.com>
2016-11-02 09:19:02 +0800

    分支

--- cf-cloud-provider/src/main/java/com/ximu/provider/ProviderApplication.java
+++ cf-cloud-provider/src/main/java/com/ximu/provider/ProviderApplication.java
@@ -1,5 +1,7 @@
 package com.ximu.provider;
-
+/**
+ * 分支
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
